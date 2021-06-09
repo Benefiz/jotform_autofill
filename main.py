550a295d1a2f05596a50352e55fed4ec533fa67c
Ekkasit <boom300742@hotmail.com>
2021-06-09 14:47:05 +0700

    add multiple generate scripts

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ import pandas as pd
 import os
 import io
 
-# Read timesheet.csv to dataframe
-timesheet = pd.read_csv("timesheet.csv", header=None)
-
 
 def createScript(timesheet, script_file_name):
-    file_name = os.path.splitext(script_file_name)[0] + '.txt'
+    file_name = os.path.splitext("script/"+script_file_name)[0] + '.txt'
     all_lines = []
     for i, row in timesheet.iterrows():
         for j, column in row.iteritems():
-            all_lines.append("document.getElementById('input_17_{}_{}').value = '{}';\n".format(i, j, column))
-    script_file = io.open(file_name, "w",encoding="utf-8")
+            all_lines.append(
+                "document.getElementById('input_17_{}_{}').value = '{}';\n".format(i, j, column))
+    script_file = io.open(file_name, "w", encoding="utf-8")
     script_file.writelines(all_lines)
     script_file.close()
 
 
-createScript(timesheet, "script")
+for file in [file for file in os.listdir("timesheet") if file.endswith('.csv')]:
+    timesheet = pd.read_csv("timesheet/"+file, header=None)
+    createScript(timesheet, os.path.splitext(file)[0])
