06a6bf4b8139d96bcbd8da0fc8def2fc03c521e6
Ekkasit <boom300742@hotmail.com>
2021-06-30 15:49:59 +0700

    add filling salary and personal information

---
+++ constant.py
@@ -0,0 +1,3 @@
+accountNo = '0123456789'
+citizenNo = '0123456789'
+salaryPerDay = 150
--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import pandas as pd
 import os
 import io
-
+import constant as c
+from num_thai.thainumbers import NumThai
 
 def createScript(timesheet, script_file_name):
+    num = NumThai()
     file_name = os.path.splitext("script/"+script_file_name)[0] + '.txt'
     all_lines = []
     for i, row in timesheet.iterrows():
         for j, column in row.iteritems():
             all_lines.append(
                 "document.getElementById('input_17_{}_{}').value = '{}';\n".format(i, j, column))
+    workDays = i+1
+    salaryPerDay = c.salaryPerDay
+    salarySum = salaryPerDay*workDays
+    salaryThaiText = num.NumberToTextThai(salarySum)
+    salaryThaiText.append("บาทถ้วน")
+    # Personal Info
+    # Account No.
+    all_lines.append("document.getElementById('input_59').value = '{}';\n".format(c.accountNo))
+    # Citizen No.
+    all_lines.append("document.getElementById('input_61').value = '{}';\n".format(c.citizenNo))
+    # Salary
+    all_lines.append("document.getElementById('input_21').value = '{}';\n".format(workDays))
+    all_lines.append("document.getElementById('input_22').value = '{}';\n".format(salaryPerDay))
+    all_lines.append("document.getElementById('input_23').value = '{}';\n".format(salarySum))
+    all_lines.append("document.getElementById('input_28').value = '{}';\n".format("".join(salaryThaiText)))
     script_file = io.open(file_name, "w", encoding="utf-8")
     script_file.writelines(all_lines)
     script_file.close()
